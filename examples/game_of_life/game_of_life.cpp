f05dc36c8131b52829e189533f303b37e071ac24
Jeroen Habraken <jhabraken@cct.lsu.edu>
2013-02-22 14:21:03 -0600

    Fixed indexing

--- examples/game_of_life/game_of_life.cpp
+++ examples/game_of_life/game_of_life.cpp
@@ -18,21 +18,20 @@ bool evolve(bool nw, bool n, bool ne,
             bool sw, bool s, bool se)
 {
     std::size_t live_neighbours = nw + n + ne + w + e + sw + s + se;
-    if (o)
-    {
-        switch (live_neighbours)
-        {
-            case 2:
-            case 3:
-                return true;
-            default:
-                return false;
-        }
+    if(live_neighbours < 2 || live_neighbours > 3){
+        return false;
     }
-    else
-    {
-        return live_neighbours == 3;
+
+    if(live_neighbours == 3){
+        return true;
     }
+
+    if (o && live_neighbours == 2)
+    {
+        return true;
+    }
+
+    return false;
 }
 HPX_PLAIN_ACTION(evolve, evolve_action)
 
@@ -49,45 +48,42 @@ bool flyer(std::size_t x, std::size_t y)
 }
 HPX_PLAIN_ACTION(flyer, flyer_action);
 
-int hpx_main(int argc, char* argv[])
+int hpx_main(boost::program_options::variables_map & vm)
 {
+    std::size_t evolutions = vm["evolutions"].as<std::size_t>();
+
     hpx::naming::id_type here = hpx::find_here();
 
-    std::array<hpx::lcos::dataflow_base<bool>, 81> grid, grid_2;
+    std::array<hpx::lcos::dataflow_base<bool>, 81> grid[2];
 
     for (std::size_t x = 0; x < 9; ++x)
     {
         for (std::size_t y = 0; y < 9; ++y)
         {
-            grid[x * 9 + y] = hpx::lcos::dataflow<flyer_action>(here, x, y);
+            grid[0][(x * 9) + y] = hpx::lcos::dataflow<flyer_action>(here, x, y);
         }
     }
 
-    std::array<hpx::lcos::dataflow_base<bool>, 81> & grid_input = grid;
-    std::array<hpx::lcos::dataflow_base<bool>, 81> & grid_output = grid_2;
-
-    for (std::size_t evolution = 0; evolution < 4; ++evolution)
+    for (std::size_t e = 0; e < evolutions; ++e)
     {
         for (std::size_t x = 0; x < 9; ++x)
         {
             for (std::size_t y = 0; y < 9; ++y)
             {
-                grid_output[x * 9 + y] = hpx::lcos::dataflow<evolve_action>(here,
-                    grid_input[((x - 1) % 9) * 9 + ((y - 1) % 9)], grid_input[((x - 1) % 9) * 9 + y], grid_input[((x - 1) % 9) * 9 + ((y + 1) % 9)],
-                    grid_input[x * 9 + ((y - 1) % 9)],             grid_input[x * 9 + y],             grid_input[x * 9 + ((y + 1) % 9)],
-                    grid_input[((x + 1) % 9) * 9 + ((y - 1) % 9)], grid_input[((x + 1) % 9) * 9 + y], grid_input[((x + 1) % 9) * 9 + ((y + 1) % 9)]
+                grid[(e + 1) % 2][(x * 9) + y] = hpx::lcos::dataflow<evolve_action>(here,
+                    grid[e % 2][((((x - 1) % 9) * 9) + ((y - 1) % 9))], grid[e % 2][((((x - 1) % 9) * 9) + y)], grid[e % 2][((((x - 1) % 9) * 9) + ((y + 1) % 9))],
+                    grid[e % 2][((x * 9)             + ((y - 1) % 9))], grid[e % 2][(x * 9)              + y],       grid[e % 2][((x * 9)             + ((y + 1) % 9))],
+                    grid[e % 2][((((x + 1) % 9) * 9) + ((y - 1) % 9))], grid[e % 2][((((x + 1) % 9) * 9) + y)], grid[e % 2][((((x + 1) % 9) * 9) + ((y + 1) % 9))]
                 );
             }
         }
-
-        std::swap(grid_input, grid_output);
     }
 
     for (std::size_t x = 0; x < 9; ++x)
     {
         for (std::size_t y = 0; y < 9; ++y)
         {
-            std::cout << grid_input[x * 9 + y].get_future().get();
+            std::cout << grid[evolutions % 2][(x * 9) + y].get_future().get();
         }
         std::cout << std::endl;
     }
@@ -97,5 +93,16 @@ int hpx_main(int argc, char* argv[])
 
 int main(int argc, char* argv[])
 {
-     return hpx::init(argc, argv);
+    boost::program_options::options_description
+        desc_commandline("usage: " HPX_APPLICATION_STRING " [options]");
+
+    desc_commandline.add_options()
+        (
+            "evolutions"
+          , boost::program_options::value<std::size_t>()->default_value(4)
+          , "Number of evolutions"
+        )
+        ;
+
+    return hpx::init(desc_commandline, argc, argv);
 }

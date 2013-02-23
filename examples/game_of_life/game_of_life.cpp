4f8490b52ce9b19df9caec6bbfd6cd3553e366e2
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-02-23 15:33:29 -0600

    Trying to fix race conditions, no real luck yet...

--- examples/game_of_life/game_of_life.cpp
+++ examples/game_of_life/game_of_life.cpp
@@ -17,6 +17,8 @@ bool evolve(bool nw, bool n, bool ne,
             bool w,  bool o, bool e,
             bool sw, bool s, bool se)
 {
+//    std::cout << "evolve: " << nw << n << ne << w << o << e << sw << s << se << std::endl;
+
     std::size_t live_neighbours = nw + n + ne + w + e + sw + s + se;
     if(live_neighbours < 2 || live_neighbours > 3){
         return false;
@@ -38,8 +40,8 @@ HPX_PLAIN_ACTION(evolve, evolve_action)
 bool flyer(std::size_t x, std::size_t y)
 {
     if ((x == 3 && y == 4) ||
-         (x == 4 && y == 5) ||
-         (x == 5 && (y == 3 || y == 4 || y == 5)))
+        (x == 4 && y == 5) ||
+        (x == 5 && (y == 3 || y == 4 || y == 5)))
     {
         return true;
     }
@@ -48,42 +50,54 @@ bool flyer(std::size_t x, std::size_t y)
 }
 HPX_PLAIN_ACTION(flyer, flyer_action);
 
+inline int make_positive(int t)
+{
+    return (t < 0) ? t + 9 : t;
+}
+
+inline std::size_t index(int x, int y)
+{
+    return (make_positive(x) % 9) * 9 + (make_positive(y) % 9);
+}
+
 int hpx_main(boost::program_options::variables_map & vm)
 {
     std::size_t evolutions = vm["evolutions"].as<std::size_t>();
 
     hpx::naming::id_type here = hpx::find_here();
 
-    std::array<hpx::lcos::dataflow_base<bool>, 81> grid[2];
+    typedef std::array<hpx::lcos::dataflow_base<bool>, 81> grid_type;
+    std::vector<grid_type> grid (evolutions+1);
 
-    for (std::size_t x = 0; x < 9; ++x)
+    for (int x = 0; x < 9; ++x)
     {
-        for (std::size_t y = 0; y < 9; ++y)
+        for (int y = 0; y < 9; ++y)
         {
-            grid[0][(x * 9) + y] = hpx::lcos::dataflow<flyer_action>(here, x, y);
+            grid[0][index(x, y)] = hpx::lcos::dataflow<flyer_action>(here, x, y);
         }
     }
 
     for (std::size_t e = 0; e < evolutions; ++e)
     {
-        for (std::size_t x = 0; x < 9; ++x)
+        for (int x = 0; x < 9; ++x)
         {
-            for (std::size_t y = 0; y < 9; ++y)
+            for (int y = 0; y < 9; ++y)
             {
-                grid[(e + 1) % 2][(x * 9) + y] = hpx::lcos::dataflow<evolve_action>(here,
-                    grid[e % 2][((((x - 1) % 9) * 9) + ((y - 1) % 9))], grid[e % 2][((((x - 1) % 9) * 9) + y)], grid[e % 2][((((x - 1) % 9) * 9) + ((y + 1) % 9))],
-                    grid[e % 2][((x * 9)             + ((y - 1) % 9))], grid[e % 2][(x * 9)              + y],       grid[e % 2][((x * 9)             + ((y + 1) % 9))],
-                    grid[e % 2][((((x + 1) % 9) * 9) + ((y - 1) % 9))], grid[e % 2][((((x + 1) % 9) * 9) + y)], grid[e % 2][((((x + 1) % 9) * 9) + ((y + 1) % 9))]
+                grid[e + 1][index(x, y)] = hpx::lcos::dataflow<evolve_action>(here,
+                    grid[e][index(x-1, y-1)], grid[e][index(x-1, y  )], grid[e][index(x-1, y+1)],
+                    grid[e][index(x  , y-1)], grid[e][index(x  , y  )], grid[e][index(x  , y+1)],
+                    grid[e][index(x+1, y-1)], grid[e][index(x+1, y  )], grid[e][index(x+1, y+1)]
                 );
             }
         }
     }
 
-    for (std::size_t x = 0; x < 9; ++x)
+    for (int x = 0; x < 9; ++x)
     {
-        for (std::size_t y = 0; y < 9; ++y)
+        for (int y = 0; y < 9; ++y)
         {
-            std::cout << grid[evolutions % 2][(x * 9) + y].get_future().get();
+//            std::cout << x << ", " << y << ": ";
+            std::cout << grid[evolutions][index(x, y)].get_future().get();
         }
         std::cout << std::endl;
     }
